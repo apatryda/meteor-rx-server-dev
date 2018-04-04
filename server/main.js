faea0842d50a07cfc60c0b5a34f447848936e584
Artur Nerkowski <artur@reaktor5.com>
2018-04-04 05:10:01 +0200

    further fixes

--- server/main.js
+++ server/main.js
@@ -1,5 +1,35 @@
+import { Collection } from 'meteor/apatryda:meteor-rx-server';
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { Observable } from 'rxjs';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  const pageCollection = new Mongo.Collection('page');
+  const pageServerCollection = new Collection(pageCollection);
+
+  pageServerCollection
+    .observe({
+      addedAt(...args) {
+        console.log('addedAt:', args);
+        return Observable.empty();
+      },
+      removedAt(...args) {
+        console.log('removedAt:', args);
+        return Observable.empty();
+      },
+    }, {}, {})
+    .subscribe(() => {})
+  ;
+
+  pageServerCollection
+    .insert({
+      title: 'Page Title',
+    })
+    .subscribe(insertId => console.log({ insertId }))
+  ;
+
+  pageServerCollection
+    .remove({})
+    .subscribe(console.log)
+  ;
 });
